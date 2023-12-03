f0904156445b7c5807f9fbac705b8d42b620c6c4
Adair Gondan <adairyves@gmail.com>
2023-12-03 17:44:30 +0100

    Dia 3 Parte1

--- day-3/src/part1.rs
+++ day-3/src/part1.rs
@@ -1,4 +1,4 @@
-use itertools::Itertools;
+use std::process::Output;
 
 use crate::custom_error::AocError;
 
@@ -10,20 +10,21 @@ pub fn process(
     let mut last: Option<&str> = None;
     let mut current = lines.next();
     let mut next = lines.next();
-    let mut suma: u32 = 0;
+    let mut suma: Vec<u32> = Vec::new();
 
     while current.is_some() {
-        process_three_lines(last, current.unwrap(), next);
+        suma.push(process_three_lines(last, current.unwrap(), next));
         last = current;
         current = next;
         next = lines.next();
     }
-
-    todo!()
+    dbg!(suma.iter().any(|f| *f > 9999));
+    Ok(suma.iter().sum::<u32>().to_string())
 }
 
 fn process_three_lines(last: Option<&str>, current: &str, next: Option<&str>) -> u32{
     let chars_act = current.chars().enumerate();
+    let chars_v =chars_act.clone().collect::<Vec<_>>();
     let mut last_digit = 0;
     let mut suma_validos: u32 = 0;
     for (index, char) in chars_act{
@@ -32,27 +33,47 @@ fn process_three_lines(last: Option<&str>, current: &str, next: Option<&str>) ->
             continue;
         }
         if char.is_ascii_digit(){
-            last_digit = 2;
+            let mut i = index;
+            let mut num_of_digits = 0;
+            while let Some((_, d)) = chars_v.get(i) {
+                if !d.is_ascii_digit(){break;}
+                num_of_digits+=1;
+                i+=1;
+            }
             let mut found = false;
             if let Some(last_line) = last {
-                found = surrounding_line_has_chars(index, last_line)
+                found = surrounding_line_has_chars(index, last_line, num_of_digits)
             }
-            if let Some(next_line) = next {
-                found = surrounding_line_has_chars(index, next_line)
+            if !found{
+                if let Some(next_line) = next {
+                    found = surrounding_line_has_chars(index, next_line, num_of_digits)
+                }
+            }
+            if !found{
+                let anterior = if index != 0 {chars_v.get(index - 1)} else {Option::None};
+                let posterior = chars_v.get(index+num_of_digits);
+                if let Some((_, d)) = anterior {
+                    if *d != '.' {found = true}
+                }
+                if let Some((_, d)) = posterior {
+                    if *d != '.' {found = true}
+                }
+                
             }
             if found {
-                suma_validos += current.get(index..index+3).unwrap().parse::<u32>().unwrap();
+                suma_validos += current.get(index..index+num_of_digits).unwrap().parse::<u32>().unwrap();
             }
+            last_digit = num_of_digits -1 ;
         }
     }
     return suma_validos;
 }
 
-fn surrounding_line_has_chars(index: usize, line: &str) -> bool{
+fn surrounding_line_has_chars(index: usize, line: &str, num_of_digits: usize) -> bool{
     let start = if index == 0 {0} else {index-1};
-    let end = if index + 3 > line.len() {line.len()} else {index + 3};
-    let chars = line.get(start..=end).unwrap().chars().filter(|c| *c != '.').try_len().unwrap();
-    chars>0
+    let end = if index + num_of_digits > (line.len() - 1) {line.len() - 1} else {index + num_of_digits};
+    let char = line.get(start..=end).unwrap().chars().filter(|c| *c != '.').next();
+    return char.is_some();
 }
 
 #[cfg(test)]
@@ -62,16 +83,16 @@ mod tests {
     #[test]
     fn test_process() -> miette::Result<()> {
         let input = "467..114..
-        ...*......
-        ..35..633.
-        ......#...
-        617*......
-        .....+.58.
-        ..592.....
-        ......755.
-        ...$.*....
-        .664.598..";
-        assert_eq!("", process(input)?);
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+        assert_eq!("4361", process(input)?);
         Ok(())
     }
 }
